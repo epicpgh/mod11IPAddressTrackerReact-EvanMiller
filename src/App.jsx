6a9791817edbb1f010f88b10851083989404e928
Evan Miller <ewmst5@gmail.com>
2025-07-05 12:39:57 -0400

    re-using some pieces from my original project, and some of the project work we did for mod 10.  Nothing testested at the moment

---
+++ src/App.jsx
@@ -0,0 +1,58 @@
+import { useState, useEffect } from 'react';
+import Header from './components/Header';
+
+import MapView from './components/MapView';
+import InfoCard from './components/InfoCard';
+import {fetchIPData} from './utils/api/fetchData';
+
+
+
+import './App.css'
+
+
+
+
+function App() {
+
+
+  const [ipData, setIpData] = useState(null);
+  const[ipQuery, setIpQuery] = useState('');
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState('');
+
+useEffect(()=>{
+  handleSearch()
+}, []);
+
+
+const handleSearch = async (ip ='') => {
+  try{
+    setLoading(true);
+    setError('');
+    const data = await fetchIPData(ip);
+    setIpData(data);
+  } catch(err) {
+    setError(err.message)
+  } finally {
+    setLoading(false);
+  }
+}
+
+  return (
+    <div className="App">
+      <Header ipQuery = {ipQuery} setIpQuery={setIpQuery} handleSearch={handleSearch} />
+      <InfoCard
+        ipData={ipData}
+        loading={loading}
+        error={error}
+      />
+      <MapView loaction={ipData?.location} loading={loading} />
+
+
+      
+    
+    </div>
+  )
+}
+
+export default App
